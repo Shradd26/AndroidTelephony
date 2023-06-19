18a66bebdacbd1abc778fc3314659ab0bc75a341
shraddha26 <kukadeshraddha26@gmail.com>
2023-06-19 14:36:05 +0530

    InitialCommit

---
+++ app/src/main/java/com/e/androidteliphony/SentActivity.java
@@ -0,0 +1,15 @@
+package com.e.androidteliphony;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sent);
+    }
+}
